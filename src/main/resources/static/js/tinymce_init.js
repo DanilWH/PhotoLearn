f681002ba6fbe614b5516995c64f357fec6635b4
DanilToLinus <danil-lomakin-02@mail.ru>
2020-11-10 20:30:27 +0500

    Add resource handlers and load the 'TinyMCE' rich text editor when necessary (not every time when a user gets to another page.

---
+++ src/main/resources/static/js/tinymce_init.js
@@ -0,0 +1,8 @@
+tinymce.init({
+    selector: 'textarea',
+    plugins: 'autoresize advcode casechange emoticons image formatpainter link lists checklist media mediaembed pageembed permanentpen powerpaste table advtable',
+    toolbar: 'bold italic strikethrough link casechange numlist bullist checklist blockquote emoticons image media pageembed permanentpen table',
+    toolbar_mode: 'floating',
+    tinycomments_mode: 'embedded',
+    tinycomments_author: 'Author name',
+});
